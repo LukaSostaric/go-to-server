3239a827237921a4e685e39aefec7058b25a8600
Luka Sostaric <luka@lukasostaric.com>
2020-09-24 11:30:11 +0200

    Always install the script for the current user.

--- install.sh
+++ install.sh
@@ -77,57 +77,58 @@ function readInput() {
     fi
 }
 function promptForBashrc() {
-    askYesOrNo "  Would you like to modify your .bashrc automatically for PATH [Y]? "
+    askYesOrNo "  Would you like to modify the .bashrc file automatically for PATH [Y]? "
     if [ $? -eq 1 ] ; then
         echo "# -- BEGIN -- Added by the go-to-server script." >> "$HOME/.bashrc"
         echo "# This can be deleted if not using the script." >> "$HOME/.bashrc"
-        echo "export PATH=$PATH:$bindir" >> "$HOME/.bashrc"
+        echo "export PATH=\$PATH:$bindir" >> "$HOME/.bashrc"
         echo "# -- END -- " >> "$HOME/.bashrc"
-        notInPath="true"
+        pathModified="true"
     fi
 }
 function promptForBashComp() {
-    askYesOrNo "  Would you like to modify your .bash_completion file automatically for autocomplete [Y]? "
+    askYesOrNo "  Would you like to modify/add your .bash_completion file automatically for autocomplete [Y]? "
     if [ $? -eq 1 ] ; then
         if [ -f "$completion" ] ; then
             echo > "$completion"
-            cat completion.sh >> "$completion"
+            cat "$sourceDir/completion.sh" >> "$completion"
         else
-            cat completion.sh > "$completion"
+            cat "$sourceDir/completion.sh" > "$completion"
         fi
         sed -i "s#SRVLISTPATH#$datafile#" "$completion"
         bcompmod="true"
     fi
 }
 # Script Start
-user=$(whoami)
-notInPath="false"
-askForBashrc="false"
-askForAutocomp="true"
-bcompmod="false"
-if [ $user = "root" ] ; then
-    echo "  WARNING: You are running this installation as the root user. That's not recommended. It's better to install Go to Server as an ordinary user."
-    destination="/opt/go-to-server"
-    configuration="/etc/opt/go-to-server/configuration"
-    datafile="/etc/opt/go-to-server/server-list"
-    bindir="/usr/local/bin"
-    completion="/usr/share/bash-completion/completions/go-to-server"
-else
-    destination="$HOME/.go-to-server"
-    configuration="$HOME/.go-to-server/configuration"
-    datafile="$HOME/.go-to-server/server-list"
-    bindir="${destination}/bin"
-    completion="$HOME/.bash_completion"
-    if [ $(echo "$PATH" | grep -c "$bindir") -eq 0 ] ; then
-        askForBashrc="true"
-    fi
+if [ $(grep -c "go-to-server" "$HOME/.bashrc") -ne 0 ] ; then
+    echo "  Error: Go to Server is already in your .bashrc file. Please remove it first before installing again."
+    exit 1
 fi
+runPath="$0"
+position="$(echo "$0" | grep -Fob "/" | tail -1 | cut -d ":" -f 1-1)"
+sourceDir="${runPath:0:position}"
+user=$(whoami)
+pathModified="false"
+bcompmod="false"
+destination="$HOME/.go-to-server"
+configuration="$HOME/.go-to-server/configuration"
+datafile="$HOME/.go-to-server/server-list"
+bindir="${destination}/bin"
+completion="$HOME/.bash_completion"
 dstPrompt="  Destination directory (default -- $destination): "
 confPrompt="  Configuration file (default -- $configuration): "
 dfprompt="  Server list file (default -- $datafile): "
 destination=$(readInput "$dstPrompt" 1 "$destination" "$user")
 configuration=$(readInput "$confPrompt" 0 "$configuration" "$user")
 datafile=$(readInput "$dfprompt" 0 "$datafile" "$user")
+if [ -f "$completion" ] && [ $(grep -c "go-to-server" "$completion") -ne 0 ] ; then
+    echo "  Error: Go to Server is already in your .bash_completion file. Please remove it first before installing again."
+    exit 1
+fi
+if [ -d "$destination" ] ; then
+    echo "Error: Go to Server is already installed in $destination! Please remove it first before installing again."
+    exit 1
+fi
 variant=1
 while true
 do
@@ -147,12 +148,8 @@ do
         echo "  Invalid choice. It can be either 1 or 2."
     fi
 done
-if [ $askForBashrc == "true" ] ; then
-    promptForBashrc
-fi
-if [ $askForAutocomp == "true" ] ; then
-    promptForBashComp
-fi
+promptForBashrc
+promptForBashComp
 position="$(echo "$destination" | grep -Fob "/" | tail -1 | cut -d ":" -f 1-1)"
 ddirectory="${destination:0:position}"
 position="$(echo "$configuration" | grep -Fob "/" | tail -1 | cut -d ":" -f 1-1)"
@@ -161,15 +158,12 @@ cdirectory="${configuration:0:position}"
 mkdir -p "$ddirectory"
 mkdir -p "$cdirectory"
 mkdir -p "$bindir"
-cp go-to-server.sh go-to-server-configured.sh go-to-server-with-expect.sh copy-from-to-server.sh "$ddirectory"
+cp "$sourceDir/go-to-server.sh" "$sourceDir/go-to-server-configured.sh" \
+    "$sourceDir/go-to-server-with-expect.sh" "$sourceDir/copy-from-to-server.sh" "$ddirectory"
 if [ ! -f "$datafile" ] ; then
-    cat server-list > "$datafile"
+    cat "$sourceDir/server-list" > "$datafile"
 fi
-if [ "$user" == "root" ] ; then
-    chmod 644 "$datafile"
-else
-    chmod 600 "$datafile"
-fi
+chmod 600 "$datafile"
 echo "DF=\"$datafile\" ## The path to a file with server information" > $configuration
 echo "VARIANT=$variant ## Variant of the program to run" >> $configuration
 content=$(cat $ddirectory/go-to-server-configured.sh)
@@ -185,12 +179,16 @@ ln -s "$ddirectory/go-to-server-configured.sh" "$bindir/copy-to-server" 2> /dev/
 if [ $? -eq 0 ] ; then
     echo "  Done. The script has been installed successfully in '$ddirectory'."
     echo "  Symbolic links were added to '$bindir'."
-    if [ "$notInPath" == "true" ] ; then
+    if [ "$pathModified" == "true" ] ; then
         echo "  The script has been added to PATH in your .bashrc file."
-        echo "  Execute 'source ~/.bashrc' to refresh your PATH."
+    else
+        echo "  Your script has not been added to PATH, and your .bashrc file has not been modified."
     fi
     if [ "$bcompmod" == "true" ] ; then
         echo "  Your .bash_completion file has been modified for autocomplete."
+    else
+        echo "  Your .bash_completion file has not been modified."
     fi
     echo "  Use the file $datafile as a template for entering your credentials."
+    echo "  Please open the new terminal window to start using the script."
 fi
